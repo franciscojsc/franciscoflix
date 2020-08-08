030be0f3ad050cbddf17431f2f4ee77bf6edffcd
Francisco Chaves <francisco.jschaves@gmail.com>
2020-08-08 16:50:09 -0300

    Aula 5

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './index.css';
-import App from './pages/Home';
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
 import CadastroVideo from './pages/cadastro/Video';
 import CadastroCategoria from './pages/cadastro/Categoria';
 
@@ -11,7 +12,7 @@ const Pagina404 = () => <div>Página 404</div>;
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
-      <Route path='/' component={App} exact />
+      <Route path='/' component={Home} exact />
       <Route path='/cadastro/video' component={CadastroVideo} exact />
       <Route path='/cadastro/categoria' component={CadastroCategoria} exact />
       <Route component={Pagina404} />
